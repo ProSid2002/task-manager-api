e4e9aa8d6f02cc052a26f0ab10db308499a99881
ProSid2002 <siddharthsharmads@gmail.com>
2021-12-18 01:34:31 +0530

    Created frontend

--- src/index.js
+++ src/index.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const userRouter = require("./routers/user");
 const taskRouter = require("./routers/task");
+const cors = require("cors");
+const path = require("path");
 require("./db/mongoose");
 
 const port = process.env.PORT;
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../public")));
+
+app.get("/", (req, res) => {
+  res.send("index.html");
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(userRouter);
 app.use(taskRouter);
