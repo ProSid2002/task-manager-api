087506da3faaa61eaa8220b0e8c732aa9cad4d20
ProSid2002 <siddharthsharmads@gmail.com>
2021-12-27 17:48:29 +0530

    Added tests

--- src/index.js
+++ src/index.js
@@ -1,23 +1,6 @@
-const express = require("express");
-const userRouter = require("./routers/user");
-const taskRouter = require("./routers/task");
-const cors = require("cors");
-const path = require("path");
-require("./db/mongoose");
+const app = require("./app");
 
 const port = process.env.PORT;
-const app = express();
-
-app.use(express.static(path.join(__dirname, "../public")));
-
-app.get("/", (req, res) => {
-  res.send("index.html");
-});
-
-app.use(cors());
-app.use(express.json());
-app.use(userRouter);
-app.use(taskRouter);
 
 app.listen(port, () => {
   console.log(`Server successfully started at port ${port}`);
